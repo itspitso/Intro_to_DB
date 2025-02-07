e5be23f87ff2b9db71ec00ec53d32da15974dd5b
itspitso <147466450+itspitso@users.noreply.github.com>
2025-02-07 17:44:36 +0200

    Made changes to the Order_Details table

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -30,7 +30,9 @@ CREATE TABLE Orders (
 
 CREATE TABLE Order_Details (
     order_detail_id PRIMARY KEY,
-    customer_id INT, 
-    order_date DATE,
-    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+    order_id INT, 
+    book_id INT,
+    quantity DOUBLE, 
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
