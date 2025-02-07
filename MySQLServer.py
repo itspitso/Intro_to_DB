c4d684b51597064e02c2f39e5c16fb8f4bf36da6
itspitso <147466450+itspitso@users.noreply.github.com>
2025-02-07 19:37:48 +0200

    Added MySQLServer.py

---
+++ MySQLServer.py
@@ -0,0 +1,20 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="yourpassword",
+    database="mydb"
+)
+
+mycursor = mydb.cursor()
+try:
+    mycursor.execute("""
+        CREATE DATABASE IF NOT EXISTS alx_book_store;
+    """
+    )
+    print("Table created successfully!")
+except:
+    print("Error")
+mycursor.close()
+mydb.close()
